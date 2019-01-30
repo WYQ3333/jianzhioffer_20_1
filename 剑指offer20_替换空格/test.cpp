77dcc83f797afbe8411ec89eef468445c777b058
浅忆梦微凉 <3538922590@qq.com>
2019-01-30 18:57:47 +0800

    不会写测试代码

--- 剑指offer20_替换空格/test.cpp
+++ 剑指offer20_替换空格/test.cpp
@@ -2,27 +2,63 @@
 using namespace std;
 #include<string>
 
+//class Solution {
+//public:
+//	void replaceSpace(char *str, int length) {
+//		string s = "";
+//		char *cur = str;
+//		while (*cur != '\0'){
+//			if (*cur != ' '){
+//				s.push_back(*cur);
+//			}
+//			if (*cur == ' '){
+//				s.push_back('%');
+//				s.push_back('2');
+//				s.push_back('0');
+//			}
+//			++cur;
+//		}
+//		s.push_back('\0');
+//		_str = const_cast<char*>(s.c_str());
+//	}
+//public:
+//	char* _str;
+//};
+
 class Solution {
 public:
 	void replaceSpace(char *str, int length) {
-		string s = "";
-		char *cur = str;
-		while (*cur != '\0'){
-			if (*cur != ' '){
-				s.push_back(*cur);
-			}
-			if (*cur == ' '){
-				s.push_back('%');
-				s.push_back('2');
-				s.push_back('0');
-			}
-			++cur;
+		if (str == NULL)
+			return;
+		int CountOfBlanks = 0;
+		int Originallength = 0;
+		for (int i = 0; str[i] != '\0'; i++)
+		{
+			Originallength++;
+			if (str[i] == ' ')
+				++CountOfBlanks;
 		}
-		s.push_back('\0');
-		_str = const_cast<char*>(s.c_str());
+		int len = Originallength + 2 * CountOfBlanks;
+		if (len + 1>length)
+			return;
+
+		char*pStr1 = str + Originallength;//复制结束符‘\0’
+		char*pStr2 = str + len;
+		while (pStr1<pStr2)
+		{
+			if (*pStr1 == ' ')
+			{
+				*pStr2-- = '0';
+				*pStr2-- = '2';
+				*pStr2-- = '%';
+			}
+			else
+			{
+				*pStr2-- = *pStr1;
+			}
+			--pStr1;
+		}
 	}
-public:
-	char* _str;
 };
 
 void TestFunc(){
@@ -30,7 +66,7 @@ void TestFunc(){
 	Solution s;
 	s.replaceSpace(str, strlen(str));
 	//strlen计算长度不包括'\0'
-	cout << s._str<< endl;
+	cout << str<< endl;
 }
 
 int main(){
